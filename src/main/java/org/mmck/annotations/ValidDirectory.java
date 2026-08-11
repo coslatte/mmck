7dda293d247a7f571a65cd914ce731689cea3070
cosmiclatte <129236813+coslatte@users.noreply.github.com>
2026-08-11 16:28:16 -0400

    some additional notes for myself

--- src/main/java/org/mmck/annotations/ValidDirectory.java
+++ src/main/java/org/mmck/annotations/ValidDirectory.java
@@ -7,6 +7,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+// @target annotation: tells java compiler that we are going to refer a function parameter
+// @retention: tells java compiler that this will save information and variables on runtime memory
+
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidDirectory {
